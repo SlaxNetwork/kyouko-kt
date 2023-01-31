6320d67eb523e74df8368cca249e28b5a77952a3
Tech <debitcardzpaypal@gmail.com>
2023-01-31 12:48:44 -0500

    feat: start work on better deployment

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,12 @@ application {
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
+ktor {
+    fatJar {
+        archiveFileName.set("kyouko-api.jar")
+    }
+}
+
 allprojects {
     repositories {
         mavenCentral()
